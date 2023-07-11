49b2025a1f93fbb6b7b846b3aee7e551c3362aff
tltzgltc <gabriellannes@id.uff.br>
2023-07-10 23:59:48 -0300

    add persistence layer

--- source/main/java/entity/Evento.java
+++ source/main/java/entity/Evento.java
@@ -1,5 +1,6 @@
 package entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,10 +8,15 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Evento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
     String nome;
     String sigla;
     String descricao;
+
+    @OneToOne
     Edicao edicao;
 }
---
+++ source/main/java/repository/EventoRepository.java
@@ -0,0 +1,9 @@
+package repository;
+
+import entity.Evento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventoRepository extends JpaRepository<Evento, Long> {
+}
